a6197f234ca9c86295e5adf7dfd522c043535377
Gaurav Jangid <60379544+gaurav700@users.noreply.github.com>
2025-02-05 17:09:28 +0000

    Adding error.js file to handle the error in meals components

---
+++ app/meals/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function Error() {
+  return (
+    <main className="not-found">
+      <h1>An error occured!</h1>
+      <p>Failed to fetch data. Please try again later!</p>
+    </main>
+  );
+}
