7a253ec1cea97923e50959aa41724c4c5e61b0cd
Gaurav Jangid <60379544+gaurav700@users.noreply.github.com>
2025-02-06 07:00:04 +0000

    Adding a server side validation on form

---
+++ app/meals/share/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function Error() {
+  return (
+    <main className="not-found">
+      <h1>An error occured!</h1>
+      <p>Failed to create meal. Please try again later!</p>
+    </main>
+  );
+}
