a14b8e474dc8c1198310bb17f7d0b17daa3d1156
Gaurav Jangid <60379544+gaurav700@users.noreply.github.com>
2025-02-06 06:28:44 +0000

    dyanmically showing button options when sharing meal

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,7 @@
 import ImagePicker from "@/app/components/meals/image-picker";
 import classes from "./page.module.css";
 import { shareMeal } from "@/lib/actions";
-
+import Button from "@/app/components/meals/button";
 export default function ShareMealPage() {
   return (
     <>
@@ -42,7 +42,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <Button />
           </p>
         </form>
       </main>
