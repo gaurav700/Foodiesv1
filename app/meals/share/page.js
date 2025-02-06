29567a19c36f65c3c497699096b27b5dcf16817f
Gaurav Jangid <60379544+gaurav700@users.noreply.github.com>
2025-02-06 00:40:14 +0000

    Creating form actions

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,5 +1,8 @@
+"use client";
+
 import classes from "./page.module.css";
 import ImagePicker from "@/app/components/meals/image-picker";
+import { shareMeal } from "@/lib/action";
 export default function ShareMealPage() {
   return (
     <>
@@ -10,7 +13,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -38,7 +41,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker name="image" label="Pick an image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
